0af586bda7f1c9349f826d475f1632ff7c1df1c1
paulmrll <paul.morille11@gmail.com>
2025-03-10 21:59:59 +0100

    update footer implementation

--- php_pages/user-info.php
+++ php_pages/user-info.php
@@ -50,8 +50,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="icon" type="image" href="../image/logo-site.webp">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/user.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/footer.css">
+
+
     <meta charset="UTF-8">
 </head>
 <body>
@@ -194,22 +194,9 @@ require_once "../php_pages/header.php";
     </div>
 </main>
 
-
-<footer>
-    <div class="footer-container">
-        <div class="contact">
-            <a href="../php_pages/contact.php" class="footer-contact">Nous contacter</a>
-            <a href="../php_pages/about-us.php" class="footer-contact">Qui sommes-nous ?</a>
-        </div>
-        <div class="socials">
-            <div>Nos réseaux :</div>
-            <a class="twitter-logo" href="https://x.com/?mx=2" target="_blank"><img src="../image/twitter-logo.png"
-                                                                                    alt="twitter-logo"></a>
-            <a class="instagram-logo" href="https://www.instagram.com/" target="_blank"><img
-                        src="../image/instagram-logo.png" alt="instagram-logo.png"></a>
-        </div>
-    </div>
-</footer>
+<?php
+require_once "../php_pages/footer.php";
+?>
 
 
 </body>
