6344ff9cf43ba20ed7759dfbb95772d9d02b7be5
paulmrll <paul.morille11@gmail.com>
2025-03-19 11:30:06 +0100

    changes

--- php_pages/user_register_travel.php
+++ php_pages/user_register_travel.php
@@ -1,26 +1,31 @@
+
 <?php
-session_start();
+require_once "../php/fonctions_utiles.php";
+
+
+if (!isset($_SESSION['email'])) {
+    header("Location: ../php_pages/connexion.php");
+    exit();
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-    <meta charset="UTF-8">
-    <title> Malte</title>
+    <title>Utilisateur</title>
     <link rel="icon" type="image" href="../image/logo-site.webp">
     <link rel="stylesheet" href="../css/styles.css">
-    <link rel="stylesheet" href="../css/destination-description.css">
+    <link rel="stylesheet" href="../css/modification.css">
 
 
+    <meta charset="UTF-8">
 </head>
-
 <body>
 
 <?php
 require_once "../php_pages/header.php";
 ?>
-
 <?php
 if (isset($_POST["destination"])){
     $destination = $_POST["destination"];
@@ -46,14 +51,16 @@ if (isset($_POST["destination"])){
 }
 ?>
 
-
 <main>
 
 
-    <div class="reservation-option-container">
-        <h2>Votre réservation à <?php echo $content[$i]["destination"]?></h2>
-        <form method="post" action="../php/register_travel.php">
-            <input type="hidden" name="destination" value="malte">
+   
+
+    <img src="<?php echo $content[$i]["image"]?>" alt="Image de la destination">
+    <h1>Commander votre voyage à <?php echo $content[$i]["destination"]?></h1>
+    <div class="container">
+    <form method="post" action="../php/modification_travel.php">
+       
 
 
             <div class="reservation-slider-container">
@@ -62,11 +69,10 @@ if (isset($_POST["destination"])){
                     <h5>Hotêl :</h5>
                     <label>
                         <select id="hotel" name="hotel" required>
-                            <?php
-                            for ($a = 0; $a < count($content[$i]['hotel']); $a++) {
-                                echo "<option value='" . $content[$i]['hotel'][$a] . "'>" . $content[$i]['hotel'][$a] . "</option>";
-                            }
-                            ?>
+                        <option value="" selected hidden>Choisissez une option</option>
+                            <?php for ($o= 0; $o < count($content[$i]["hotel"]); $o++):?>
+                            <option value="<?php echo $content[$i]["hotel"][$o]?>"><?php echo $content[$i]["hotel"][$o]?></option>
+                            <?php endfor;?>
                         </select>
                     </label>
                 </div>
@@ -75,32 +81,22 @@ if (isset($_POST["destination"])){
                 <div class="reservation-checkbox">
                     <h5>Activités nautiques :</h5>
                     <div>
-                        <?php
-                        for ($a = 0; $a < count($content[$i]['loisir']); $a++):
-                           
-                        ?>
-                        <input type="checkbox" id="loisir<?php echo"$a"?>" name="loisir[]" value="<?php echo $content[$i]['loisir'][$a] ?>">
-                        <label class="reservation-button" for="loisir<?php echo"$a"?>"><?php echo $content[$i]['loisir'][$a] ?></label>
+                    <?php for ($o = 0; $o < count($content[$i]["loisir"]); $o++): ?>
+                    <input type="checkbox" id="loisir<?php echo $o ?>" name="loisir[]" value="<?php echo $content[$i]["loisir"][$o] ?>">
+                    <label class="reservation-button" for="loisir<?php echo $o ?>"><?php echo $content[$i]["loisir"][$o] ?></label>
+                    <?php endfor; ?>
 
-                        <?php
-                        endfor;
-                        ?>
+                    </div>
                 </div>
 
 
                 <div class="reservation-checkbox">
                     <h5>Visite guidée :</h5>
                     <div>
-                    <?php
-                        for ($a = 0; $a < count($content[$i]['visite']); $a++):
-                           
-                        ?>
-                        <input type="checkbox" id="visite<?php echo"$a"?>" name="visite[]" value="<?php echo $content[$i]['visite'][$a] ?>">
-                        <label class="reservation-button" for="visite<?php echo"$a"?>"><?php echo $content[$i]['visite'][$a] ?>s</label>
-
-                        <?php
-                        endfor;
-                        ?>
+                    <?php for ($o = 0; $o < count($content[$i]["visite"]); $o++): ?>
+                    <input type="checkbox" id="visite<?php echo $o ?>" name="visite[]" value="<?php echo $content[$i]["visite"][$o] ?>">
+                    <label class="reservation-button" for="visite<?php echo $o ?>"><?php echo $content[$i]["visite"][$o] ?></label>
+                    <?php endfor; ?>
                     </div>
                 </div>
 
@@ -108,22 +104,16 @@ if (isset($_POST["destination"])){
                 <div class="reservation-checkbox">
                     <h5>Activité de détentes :</h5>
                     <div>
-                    <?php
-                        for ($a = 0; $a < count($content[$i]['relaxation']); $a++):
-                           
-                        ?>
-                        <input type="checkbox" id="détente<?php echo"$a"?>" name="relaxation[]" value="<?php echo $content[$i]['relaxation'][$a] ?>">
-                        <label class="reservation-button" for="détente<?php echo"$a"?>"><?php echo $content[$i]['relaxation'][$a] ?></label>
-
-                        <?php
-                        endfor;
-                        ?>
+                    <?php for ($o = 0; $o < count($content[$i]["relaxation"]); $o++): ?>
+                    <input type="checkbox" id="relaxation<?php echo $o ?>" name="relaxation[]" value="<?php echo $content[$i]["relaxation"][$o] ?>">
+                    <label class="reservation-button" for="relaxation<?php echo $o ?>"><?php echo $content[$i]["relaxation"][$o] ?></label>
+                    <?php endfor; ?>
                     </div>
                 </div>
 
                 <div class="reservation-checkbox">
                     <h5>Dates de départ :</h5>
-                    <input id="departure" name="departure" type="date" placeholder="jj/mm/aaaa" required></td>
+                    <input id="departure" name="departure" type="date" placeholder="jj/mm/aaaa" required>
 
                 </div>
 
@@ -131,7 +121,7 @@ if (isset($_POST["destination"])){
                 <div class="reservation-checkbox">
 
                     <h5>Dates de retour :</h5>
-                    <input id="return" name="return" type="date" placeholder="jj/mm/aaaa" required></td>
+                    <input id="return" name="return" type="date" placeholder="jj/mm/aaaa" required>
 
                 </div>
 
@@ -140,17 +130,25 @@ if (isset($_POST["destination"])){
                 </div>
 
             </div>
-            <br><br><br>
             <div class="button-container" id="buttons">
-                <button type="submit">Réserver</button>
-                <button type="reset">Reset</button>
+                <input type="hidden" name="todo" value="modify">
+                <button type="submit">Modifier</button>
+
             </div>
         </form>
+        <form method="post" action="../php/modification_travel.php">
+        <input type="hidden" name="todo" value="delete">
+        <button type="submit" id="delete">Supprimer le voyage</button>
+        </form>
     </div>
+</main>
+
 
 <?php
 require_once "../php_pages/footer.php";
 ?>
-</body>
 
+
+</body>
 </html>
+
