d73a46568e86a8c8bc39cdcb570a35f71917b1e7
FLG95 <florian.goupil@icloud.com>
2025-03-21 11:24:28 +0100

    update

--- php_pages/description-pages.php
+++ php_pages/description-pages.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_GET["destination"])){
+if (!isset($_GET["destination"])) {
     header('Location: ../php_pages/destination.php');
     exit();
 }
@@ -8,25 +8,25 @@ $destination = $_GET["destination"];
 $destination = strtolower($destination);
 $destination[0] = strtoupper($destination[0]);
 $jsonFile = "../data/travel.json";
-if (!file_exists($jsonFile)){
+if (!file_exists($jsonFile)) {
     header('Location: ../php_pages/add_travel.php');
     exit();
 }
 $content = json_decode(file_get_contents($jsonFile), true);
-if ($content == null){
+if ($content == null) {
     header('Location: ../php_pages/add_travel.php');
     exit();
 }
 $a = 0;
 $i = -1;
-for ($t = 0; $t < count($content); $t++){
-    if ($content[$t]['destination'] === $destination){
+for ($t = 0; $t < count($content); $t++) {
+    if ($content[$t]['destination'] === $destination) {
         $a = 1;
         $i = $t;
         break;
     }
 }
-if ($a === 0){
+if ($a === 0) {
     header('Location: ../php_pages/add_travel.php');
     exit();
 }
@@ -36,7 +36,7 @@ if ($a === 0){
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title><?php echo $destination?></title>
+    <title><?php echo $destination ?></title>
     <link rel="icon" type="image" href="../image/logo-site.webp">
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="stylesheet" href="../css/destination-description.css">
@@ -58,15 +58,15 @@ require_once "../php_pages/header.php";
 
     <div class="destination-text">
         <p>
-            <?php echo $content[$i]['description'];?>
+            <?php echo $content[$i]['description']; ?>
         </p>
     </div>
 
-    <a href="user_register_travel.php?destination=<?php echo $content[$i]['destination'];?>">Réserver</a>
+    <a class="button-reserver" href="user_register_travel.php?destination=<?php echo $content[$i]['destination']; ?>">Réserver</a>
 
-    
+
     <div class="maps">
-        <img src="<?php echo $content[$i]["image_maps"]?>" alt="<?php echo $content[$i]["destination"]?>-maps">
+        <img src="<?php echo $content[$i]["image_maps"] ?>" alt="<?php echo $content[$i]["destination"] ?>-maps">
         <br>
         <p>Carte <?php echo $content[$i]["destination"] ?></p>
     </div>
