c781e9ade8943cf29c1639f5193555e262868a9d
paulmrll <paul.morille11@gmail.com>
2025-05-15 15:45:16 +0200

    Update user_register_travel.js

--- js/user_register_travel.js
+++ js/user_register_travel.js
@@ -7,11 +7,61 @@ function minimumDate() {
     
 }
 
+function validateForm() {
+    let inputs = document.querySelectorAll("input");
+    let hotel = document.querySelector("#hotel");
+    let submitButton = document.querySelector("button[type='submit']");
+    submitButton.disabled = true;
+    let numberOfChecked1 = 0;
+    let numberOfChecked2 = 0;
+    let numberOfChecked3 = 0;
+    let number4 = 0;
+    let number5 = 0;
+    let number6 = 0;
+    for (let i = 0; i < inputs.length; i++){
+        if (inputs[i].value === ""){
+            number5++;
+        } else {
+            if (inputs[i].type === "checkbox" ) {
+                switch (inputs[i].name) {
+                    case "visite[]":
+                        if (inputs[i].checked) {
+                            numberOfChecked1++;
+                        }
+                        break;
+                    case "relaxation[]":
+                        if (inputs[i].checked) {
+                            numberOfChecked2++;
+                        }
+                        break;
+                    case "loisir[]":
+                        if (inputs[i].checked) {
+                            numberOfChecked3++;
+                        }
+                        break;
+                }
+            } else if (inputs[i].type === "number") {
+                if (inputs[i].value <= 0) {
+                    number4++;
+                }
+            }
+        }
+        if (hotel.value === ""){
+            number6++;
+        }
+        if (numberOfChecked1 == 0 || numberOfChecked2 == 0 || numberOfChecked3 == 0 || number4 > 0 || number5 > 0 || number6 > 0) {
+            submitButton.disabled = true;
+        } else {
+            submitButton.disabled = false;
+        }
+
+    }
+}
+
 
 
 
 async function price(){
-    const price = document.querySelector("#prix_final");
     const form = document.getElementById("form1");
     const formData = new FormData(form);
     const prix = document.querySelector("#prix_final");
@@ -33,23 +83,28 @@ async function price(){
         console.log("Erreur HTTP :", response.status);
     }
 }
-let inputs = document.querySelectorAll("input");
-let hotel = document.querySelector("#hotel");
-let submitButton = document.querySelector("button[type='submit']");
-submitButton.disabled = false;
-for (let j = 0; j < inputs.length; j++) {
-    inputs[j].addEventListener("input", ()=>{
-        price();
 
-    });
-}
-hotel.addEventListener("change", () => {
-    price();
-}
-);
+
+
+
 
 
 window.addEventListener("DOMContentLoaded", () => {
+    let inputs = document.querySelectorAll("input");
+    let hotel = document.querySelector("#hotel");
+    let submitButton = document.querySelector("button[type='submit']");
+    submitButton.disabled = true;
     minimumDate();
     document.getElementById("departure").addEventListener("change", minimumDate);
+    hotel.addEventListener("change", () => {
+        price();
+        validateForm();
+    });
+    for (let j = 0; j < inputs.length; j++) {
+    inputs[j].addEventListener("input", ()=>{
+        price();
+        validateForm();
+
+    });
+}
 });
